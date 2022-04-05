efde4f6d9bfe7e6159e9437a9dd8767755102b34
Caio Antonio <caio.antonio.c@outlook.com>
2022-04-05 20:37:48 -0300

    first commit

--- cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/CooperativeVotesApplication.java
+++ cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/CooperativeVotesApplication.java
@@ -1,17 +1,17 @@
-package edu.mentorship.cooperative;
+package edu.mentorship.cooperativevotes;
 
-import edu.mentorship.cooperative.structure.config.properties.KafkaProperties;
-import edu.mentorship.cooperative.structure.config.properties.RedisProperties;
+import edu.mentorship.cooperativevotes.structure.config.properties.KafkaProperties;
+import edu.mentorship.cooperativevotes.structure.config.properties.RedisProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {KafkaProperties.class, RedisProperties.class})
-public class CooperativeApplication {
+public class CooperativeVotesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CooperativeApplication.class, args);
+		SpringApplication.run(CooperativeVotesApplication.class, args);
 	}
 
 }
