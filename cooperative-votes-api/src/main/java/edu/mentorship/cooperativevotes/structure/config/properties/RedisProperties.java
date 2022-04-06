156ecb420eec00cf1a3fdec95f7343c16dc4699b
Caio Roberto <caio.roberto@sensedia.com>
2022-04-06 10:16:26 -0300

    docs: created openapi

--- cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/structure/config/properties/RedisProperties.java
+++ cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/structure/config/properties/RedisProperties.java
@@ -2,10 +2,12 @@ package edu.mentorship.cooperativevotes.structure.config.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Validated
 @ConfigurationProperties(prefix="cooperative-votes-api.configurations.redis")
 public class RedisProperties {
     @NotBlank(message="host is required.")
