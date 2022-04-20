b5284c39988f9e0d39ac8b4a9b104e947f39a3ad
Caio Roberto <caio.roberto@sensedia.com>
2022-04-20 09:16:20 -0300

    feat: created stave crud

--- cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/application/endpoints/StaveEndpoint.java
+++ cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/application/endpoints/StaveEndpoint.java
@@ -2,11 +2,10 @@ package edu.mentorship.cooperativevotes.application.endpoints;
 
 import edu.mentorship.cooperativevotes.application.api.StaveApi;
 import edu.mentorship.cooperativevotes.application.dto.InputNewStaveDto;
+import edu.mentorship.cooperativevotes.application.dto.InputUpdateStaveDto;
 import edu.mentorship.cooperativevotes.application.dto.StaveDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/staves")
@@ -16,5 +15,17 @@ public class StaveEndpoint implements StaveApi {
     @PostMapping
     public ResponseEntity<StaveDto> create(InputNewStaveDto inputNewStaveDto) {
         return StaveApi.super.create(inputNewStaveDto);
+    }
+
+    @Override
+    @PutMapping
+    public ResponseEntity<StaveDto> update(Long id, InputUpdateStaveDto inputUpdateStaveDto) {
+        return StaveApi.super.update(id, inputUpdateStaveDto);
+    }
+
+    @Override
+    @DeleteMapping
+    public ResponseEntity<Void> cancel(Long id) {
+        return StaveApi.super.cancel(id);
     }
 }
