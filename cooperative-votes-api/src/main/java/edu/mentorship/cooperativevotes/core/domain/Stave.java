d52bb5b4b0bdc6eb67dba7df9df9ca8d5eac5c24
Caio Roberto <caio.roberto@sensedia.com>
2022-05-02 16:57:32 -0300

    fix: updated domain

--- cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/core/domain/Stave.java
+++ cooperative-votes-api/src/main/java/edu/mentorship/cooperativevotes/core/domain/Stave.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Data
 @Document(collection = "stave")
@@ -33,11 +32,11 @@ public class Stave {
     private int totalVoteInvalid;
 
     public void createAt() {
-        createAt = LocalDateTime.now(ZoneId.of("UTC"));
+        createAt = LocalDateTime.now();
         updateAt();
     }
 
     public void updateAt() {
-        updateAt = LocalDateTime.now(ZoneId.of("UTC"));
+        updateAt = LocalDateTime.now();
     }
 }
